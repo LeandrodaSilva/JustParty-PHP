d3d6cbea7cfb22424e8da1fef2288fcdb18c108c
LeandrodaSilva <ld_silva13@hotmail.com>
2019-11-09 21:34:03 -0300

    Adicionado bootstrap, novas rotas e l5-repository

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -10,4 +10,11 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+
+    function login() {
+        $tittle = "Login";
+        return view('login', [
+            'tittle' => $tittle
+        ]);
+    }
 }
